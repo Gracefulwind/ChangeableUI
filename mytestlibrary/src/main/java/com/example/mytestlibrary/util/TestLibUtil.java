d20f0635dcfaa8f43524cf0a578a90736d66c9a2
Gracefulwind <Gracefulwindbigwang@gmail.com>
2020-04-17 15:06:17 +0800

    [chg] 测试jitpack依赖

---
+++ mytestlibrary/src/main/java/com/example/mytestlibrary/util/TestLibUtil.java
@@ -0,0 +1,22 @@
+package com.example.mytestlibrary.util;
+
+/**
+ * @ClassName: TestLibUtil
+ * @Author: Gracefulwind
+ * @CreateDate: 2020/4/17 15:03
+ * @Description: ---------------------------
+ * @UpdateUser:
+ * @UpdateDate: 2020/4/17 15:03
+ * @UpdateRemark:
+ * @Version: 1.0
+ * @Email: 429344332@qq.com
+ */
+
+public class TestLibUtil {
+
+    public static void testLib(){
+        System.out.println("==上传依赖lib成功==");
+        System.out.println("==上传依赖lib成功==");
+        System.out.println("==上传依赖lib成功==");
+    }
+}
